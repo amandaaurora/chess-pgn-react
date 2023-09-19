8a465a7aa44f073f70173290c2edd057af0b016d
Amanda Aurora <amandeaurora@gmail.com>
2023-09-19 15:08:09 +0200

    kickstart project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route } from "react-router-dom";
+import { ChessProvider } from './contexts/ChessContext';
+
+import GetImageLayout from './components/GetImageLayout';
+
+function App() {
+  return (
+
+    <div className="App">
+
+          <ChessProvider>
+          <Routes>
+
+            {/* <Route path="/upload" element= { <UploadImageLayout/>}/> */}
+            <Route path="/" element= { <GetImageLayout/>}/>
+          </Routes>
+          </ChessProvider>
+
+          {/* <PlayRandomMoveEngine /> */}
+
+      <footer className='footer navbar-custom'>
+          <p>Lali Bibilashvili, Amanda Aurora, David Rosillo</p>
+          <p>2023</p>
+        </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="chess-piece-spinner">
+    <img src= {require('./Picture3.png')} alt="Spinning Chess Piece" />
+
+  </div>
+  );
+}
+
+export default Spinner;
